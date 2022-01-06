5e06639c132539051be762b78ae34f1a230acd70
chitwan kasana <chitwan.kasana@quokkalabs.com>
2022-01-06 14:31:05 +0530

    test store deployment

--- app/src/main/java/com/example/githubworkflowssample/MainActivity.kt
+++ app/src/main/java/com/example/githubworkflowssample/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.githubworkflowssample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        var lottieAnimView: LottieAnimationView = findViewById(R.id.animView)
+        lottieAnimView.playAnimation()
 
 
 
