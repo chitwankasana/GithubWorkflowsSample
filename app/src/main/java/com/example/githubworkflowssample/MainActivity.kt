6844c4086f431c054e0ad0b9f3a0420cee8268eb
chitwan kasana <chitwan.kasana@quokkalabs.com>
2022-01-10 18:17:27 +0530

    add webview

--- app/src/main/java/com/example/githubworkflowssample/MainActivity.kt
+++ app/src/main/java/com/example/githubworkflowssample/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.githubworkflowssample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import com.airbnb.lottie.LottieAnimationView
+import android.webkit.WebViewClient
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +15,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        var lottieAnimView: LottieAnimationView = findViewById(R.id.animView)
-        lottieAnimView.playAnimation()
+//        var lottieAnimView: LottieAnimationView = findViewById(R.id.animView)
+//        lottieAnimView.playAnimation()
 
+        var webView: NoSuggestionsWebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.loadUrl("https://www.google.com")
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                webView.loadUrl("javascript:document.body.style.margin=\"8%\"; void 0")
+            }
+        }
 
 
 
